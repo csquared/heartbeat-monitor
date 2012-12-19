64bc58639b7aa8461e1ed5f0194df98f9f34b6ed
Chris Continanza <christopher.continanza@gmail.com>
2012-12-19 14:15:08 -0800

    cleanup test

--- test/arduino_test.rb
+++ test/arduino_test.rb
@@ -7,14 +7,14 @@ class ArduinoTest < Vault::TestCase
   def test_heartbeat_uses_heartbeat_delay
     set_env 'HEARTBEAT_DELAY', '0'
     Arduino.up?.must_equal false
-    Arduino.heartbeat
+    lambda { Arduino.heartbeat }.must_output /heartbeat/,''
     Arduino.up?.must_equal false
   end
 
   # Arduino.heartbeat should make Arduino.up? true
   def test_heartbeat_sets_up_to_true
     Arduino.up?.must_equal false
-    Arduino.heartbeat
+    lambda { Arduino.heartbeat }.must_output /heartbeat/,''
     Arduino.up?.must_equal true
   end
 end
